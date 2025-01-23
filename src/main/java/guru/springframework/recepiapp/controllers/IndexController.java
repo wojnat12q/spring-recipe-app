4117ac216834a20b25ee6ee8b75644d908779460
wojnat12q <wojteknato@gmail.com>
2025-01-23 15:21:25 +0100

    comit and push

---
+++ src/main/java/guru/springframework/recepiapp/controllers/IndexController.java
@@ -0,0 +1,13 @@
+package guru.springframework.recepiapp.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+        @RequestMapping({"","/","/index"})
+        public String getIndexPage() {
+            System.out.println("say hello aaaa");
+            return "index";
+        }
+}
