50ee10d5eb3fc944c0bf5adfe33cbaf3e17521e9
wojnat12q <wojteknato@gmail.com>
2025-01-23 13:31:45 +0100

    comit and push

---
+++ src/main/java/guru/springframework/recepiapp/RecepiAppApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.recepiapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecepiAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecepiAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/guru/springframework/recepiapp/RecepiAppApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.recepiapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecepiAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
