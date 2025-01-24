30cc71b7d4394690cfe0ba3bac238c0eb5c426b9
wojnat12q <wojteknato@gmail.com>
2025-01-24 13:45:46 +0100

    index

--- src/main/java/guru/springframework/services/RecipeServiceImpl.java
+++ src/main/java/guru/springframework/services/RecipeServiceImpl.java
@@ -2,6 +2,7 @@ package guru.springframework.services;
 
 import guru.springframework.domain.Recipe;
 import guru.springframework.repositories.RecipeRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.HashSet;
@@ -10,6 +11,7 @@ import java.util.Set;
 /**
  * Created by jt on 6/13/17.
  */
+@Slf4j
 @Service
 public class RecipeServiceImpl implements RecipeService {
 
@@ -21,6 +23,7 @@ public class RecipeServiceImpl implements RecipeService {
 
     @Override
     public Set<Recipe> getRecipes() {
+        log.debug("i like getRecipes");
         Set<Recipe> recipeSet = new HashSet<>();
         recipeRepository.findAll().iterator().forEachRemaining(recipeSet::add);
         return recipeSet;
