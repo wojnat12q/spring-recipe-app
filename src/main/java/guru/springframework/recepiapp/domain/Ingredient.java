b789fb310238df2af6cea12000f9bb9772b23341
wojnat12q <wojteknato@gmail.com>
2025-01-24 09:17:53 +0100

    comit and push

---
+++ src/main/java/guru/springframework/recepiapp/domain/Ingredient.java
@@ -0,0 +1,36 @@
+package guru.springframework.recepiapp.domain;
+
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+@Entity
+public class Ingredient {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String description;
+    private BigDecimal amount;
+
+    @OneToOne(fetch = FetchType.EAGER)
+    private UnitOfMeasure uom;
+
+    @ManyToOne
+    private Recipe recipe;
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
