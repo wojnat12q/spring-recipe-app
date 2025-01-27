135a108c2a9d1df128e4e43f34f3c3f1fe8e36fd
wojnat12q <wojteknato@gmail.com>
2025-01-27 12:08:16 +0100

    index

--- src/main/java/guru/springframework/controllers/IndexController.java
+++ src/main/java/guru/springframework/controllers/IndexController.java
@@ -1,30 +1,29 @@
 package guru.springframework.controllers;
 
-import guru.springframework.domain.Category;
-import guru.springframework.domain.UnitOfMeasure;
-import guru.springframework.repositories.CategoryRepository;
-import guru.springframework.repositories.UnitOfMeasureRepository;
+import guru.springframework.services.RecipeService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.Optional;
 
+@Slf4j
 @Controller
 public class IndexController {
-        private CategoryRepository categoryRepository;
-        private UnitOfMeasureRepository unitOfMeasureRepository;
 
-    public IndexController(CategoryRepository categoryRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
-        this.categoryRepository = categoryRepository;
-        this.unitOfMeasureRepository = unitOfMeasureRepository;
+    private final RecipeService recipeService;
+
+    public IndexController(RecipeService recipeService) {
+        this.recipeService = recipeService;
     }
 
-    @RequestMapping({"","/","/index"})
-        public String getIndexPage() {
-            Optional<Category> categoryOptional = categoryRepository.findByDescription("American");
-            Optional<UnitOfMeasure> unitOfMeasureOptional = unitOfMeasureRepository.findByDescription("Cup");
+    @RequestMapping({"", "/", "/index"})
+    public String getIndexPage(Model model) {
+        log.debug("Getting Index page");
 
-        //System.out.println("Cat Id is: " + categoryOptional.get().getId()+"\nUnit Of Measure is: " + unitOfMeasureOptional.get().getId());
-            return "index";
-        }
+        model.addAttribute("recipes", recipeService.getRecipes());
+
+        return "index";
+    }
 }
---
+++ src/test/java/guru/springframework/repositories/UnitOfMeasureRepositoryTest.java
@@ -0,0 +1,33 @@
+package guru.springframework.repositories;
+
+
+import guru.springframework.domain.UnitOfMeasure;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+class UnitOfMeasureRepositoryTest {
+    @Autowired
+    UnitOfMeasureRepository unitOfMeasureRepository;
+
+    @BeforeEach
+    void setUp() throws Exception {
+
+    }
+
+    @Test
+    void findByDescription() throws Exception {
+        Optional<UnitOfMeasure> unitOfMeasure = unitOfMeasureRepository.findByDescription("Teaspoon");
+
+        assertEquals("Teaspoon", unitOfMeasure.get().getDescription());
+    }
+}
