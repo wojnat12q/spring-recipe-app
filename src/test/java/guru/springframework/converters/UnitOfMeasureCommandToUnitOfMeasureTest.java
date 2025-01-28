d76d2a0694297036e8e86f21018245cd80ee9b65
wojnat12q <wojteknato@gmail.com>
2025-01-28 12:12:54 +0100

    index

--- src/test/java/guru/springframework/converters/UnitOfMeasureCommandToUnitOfMeasureTest.java
+++ src/test/java/guru/springframework/converters/UnitOfMeasureCommandToUnitOfMeasureTest.java
@@ -10,7 +10,7 @@ import static org.junit.jupiter.api.Assertions.*;
 public class UnitOfMeasureCommandToUnitOfMeasureTest {
 
     public static final String DESCRIPTION = "description";
-    public static final Long LONG_VALUE = new Long(1L);
+    public static final Long LONG_VALUE = 1L;
 
     UnitOfMeasureCommandToUnitOfMeasure converter;
 
--- src/test/java/guru/springframework/converters/UnitOfMeasureToUnitOfMeasureCommandTest.java
+++ src/test/java/guru/springframework/converters/UnitOfMeasureToUnitOfMeasureCommandTest.java
@@ -12,7 +12,7 @@ import static org.junit.jupiter.api.Assertions.*;
 public class UnitOfMeasureToUnitOfMeasureCommandTest {
 
     public static final String DESCRIPTION = "description";
-    public static final Long LONG_VALUE = new Long(1L);
+    public static final Long LONG_VALUE = 1L;
 
     UnitOfMeasureToUnitOfMeasureCommand converter;
 
